cf863009bf433962f4d0d5284d970c203b9775b4
Kynce <137978684+kevinatgit23@users.noreply.github.com>
2024-01-17 09:51:10 +0800

    sdd

--- script.js
+++ script.js
@@ -53,3 +53,24 @@ document.addEventListener('DOMContentLoaded', function () {
       imagePreview.style.display = "none";
     }, 300); // Adjust the delay to match the transition duration
   }
+
+//darkmode
+
+  var checkbox = document.querySelector('input[name=mode]');
+
+  checkbox.addEventListener('change', function() {
+      if(this.checked) {
+          trans()
+          document.documentElement.setAttribute('data-theme', 'dark')
+      } else {
+          trans()
+          document.documentElement.setAttribute('data-theme', 'light')
+      }
+  })
+
+  let trans = () => {
+      document.documentElement.classList.add('transition');
+      window.setTimeout(() => {
+          document.documentElement.classList.remove('transition');
+      }, 1000)
+  }
